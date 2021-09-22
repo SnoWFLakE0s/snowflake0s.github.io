31d99a4f35ad601393d43eb97ca516b10c551dd6
Joshua Eah <inventor1234567890@gmail.com>
2021-09-22 00:48:04 -0400

    more testing

--- library/collapsetoggle.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  var $groupOne = $('#myGroup');
-    $groupOne.on('show.bs.collapse','.collapse', function() {
-    $groupOne.find('.collapse.show').collapse('hide');
-  });
-});
